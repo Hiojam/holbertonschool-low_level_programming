f0fd775f4edb8422c65597249635be04930165e1
Roxxion <hiojam2016@gmail.com>
2023-02-15 09:52:40 -0800

    Task 1: Betty: some style fixed

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -41,6 +41,7 @@ int main(void)
 char *concat(const char *s1, const char *s2)
 {
 	char *result = malloc(strlen(s1) + strlen(s2) + 1);
+
 	strcpy(result, s1);
 	strcat(result, s2);
 	return (result);
