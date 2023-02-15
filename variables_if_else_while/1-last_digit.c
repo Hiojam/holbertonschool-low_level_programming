8543b2b6ef92f4bb1c57c0f604117e41557c9fd8
Roxxion <hiojam2016@gmail.com>
2023-02-15 09:41:39 -0800

    Task 1: Betty: some style fixed

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 /* more headers goes there */
 
-char* concat(const char*, const char*);
+char *concat(const char*, const char*);
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -16,22 +16,24 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	nLastDigit = n%10;
+	nLastDigit = n % 10;
 
 	text = "Last digit of %d is %d ";
-	
-	if(nLastDigit > 5){
-		text = concat(text, "and is greater than 5");
+	if(nLastDigit > 5)
+	{
+		text = concat( text, "and is greater than 5" );
 	}
-	else if(nLastDigit == 0){
-		text = concat(text, "and is 0");
+	else if( nLastDigit == 0 )
+	{
+		text = concat( text, "and is 0" );
 	}
-	else if(nLastDigit < 6){
-		text = concat(text, "and is less than 6 and not 0");
+	else if( nLastDigit < 6 )
+	{
+		text = concat( text, "and is less than 6 and not 0" );
 	}
-	text = concat(text, "\n");
+	text = concat( text, "\n" );
 
-	printf(text, n, nLastDigit);
+	printf( text, n, nLastDigit );
 
 	return (0);
 }
@@ -41,5 +43,5 @@ char* concat(const char *s1, const char *s2)
     char *result = malloc(strlen(s1) + strlen(s2) + 1);
     strcpy(result, s1);
     strcat(result, s2);
-    return result;
+    return (result);
 }
