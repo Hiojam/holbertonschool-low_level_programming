d8e37b4de2601019c4f06468c23f0fa4283daf6c
Roxxion <hiojam2016@gmail.com>
2023-02-16 08:37:46 -0800

    Task 7

---
+++ functions_nested_loops/autoCreateFunctionCSintax.sh
@@ -0,0 +1,36 @@
+echo "Please type file name"
+read fileName
+
+echo "Please type function name"
+read funcName
+
+echo "Please type function description"
+read funcDesc
+
+echo "Please type param type"
+read paramType
+
+echo "Please type the return description"
+read retDesc
+
+echo "Please type function type"
+read funcType
+
+cat <<EOT >> $fileName
+#include "main.h"
+
+/**
+ * $funcName - $funcDesc
+ *
+ * @n: $paramType
+ * Return: $retDesc
+ */
+$funcType $funcName($paramType n)
+{
+    return ();
+}
+EOT
+
+echo "$funcType $funcName($paramType);" >> main.h
+
+echo "File created successfuly."
