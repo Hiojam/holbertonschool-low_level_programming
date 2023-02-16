6d865f07a555dbc65d7573f090b56d947e5aa95a
Roxxion <hiojam2016@gmail.com>
2023-02-16 05:41:11 -0800

    Task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,5 @@
 #include "main.h"
 
-void print_alphabet(void);
-
-/**
- * main - Main function
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 /**
  * print_alphabet - Prints the alphabet
  *
