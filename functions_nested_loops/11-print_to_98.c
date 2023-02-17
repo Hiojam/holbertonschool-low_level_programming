a7b419e06358816bb3ea142c3a68255607475261
Roxxion <hiojam2016@gmail.com>
2023-02-17 07:16:44 -0800

    Task 11: changed printf to custom func

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void printnumber(int);
+
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  *
@@ -11,20 +13,39 @@ void print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			printf("%d", n);
+			printnumber(n);
 			_putchar(',');
 			_putchar(' ');
 		}
-		printf("%d", 98);
-		printf("\n");
+		printnumber(98);
+		_putchar('\n');
 		return;
 	}
 
 	for (; n < 98 ; n++)
 	{
-		printf("%d", n);
+		printnumber(n);
 		_putchar(',');
 		_putchar(' ');
 	}
-	printf("%d\n", 98);
+	_putchar('\n');
+}
+
+/**
+ * printnumber - prints a number
+ * @n: number to print
+ */
+void printnumber(int n)
+{
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n / 10)
+		printnumber(n / 10);
+
+	_putchar(n % 10 + '0');
 }
