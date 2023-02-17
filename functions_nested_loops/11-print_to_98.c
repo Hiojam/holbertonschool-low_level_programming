304e69b5583539b285c6c394abda5ee26307b2ea
Roxxion <hiojam2016@gmail.com>
2023-02-17 07:06:22 -0800

    Task 11: reverse added

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,6 +7,16 @@
  */
 void print_to_98(int n)
 {
+	if (n > 98)
+	{
+		for (; n > 98; n++)
+		{
+			printf("%d", n);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
 	for (; n < 98 ; n++)
 	{
 		printf("%d", n);
