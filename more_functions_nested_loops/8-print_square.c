7a2d2a608d29942a0c1321c2c9f391a42c132af2
Roxxion <hiojam2016@gmail.com>
2023-02-22 07:04:09 -0800

    Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line
+ *
+ * @n: int
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int times;
+	int z;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (times = 0; times < n; times++)
+	{
+		for (z = 0; z < n; z++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
