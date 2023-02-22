f06aee0b7857e068fe447bcd0537974793d5c1f2
Roxxion <hiojam2016@gmail.com>
2023-02-22 06:18:04 -0800

    Task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+
+		printf("%d", num);
+	}
+}
