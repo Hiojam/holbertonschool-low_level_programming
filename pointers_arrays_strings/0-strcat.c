0956caa151040abd4986f0ac4c3a2ea62d066ee3
Roxxion <hiojam2016@gmail.com>
2023-02-28 05:20:45 -0800

    Task 0: Fixed betty and removed test main

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int main(){ *_strcat("Hola ", "como estas"); return 0;}
-
 /**
  * *_strcat - Appends the src string to the dest string
  *
@@ -17,12 +15,12 @@ char *_strcat(char *dest, char *src)
 	for (; *dest; dest++)
 		destLen++;
 
-    while (src[i])
-    {
-        if (src[i] != '\0')
-            dest[destLen + i] = src[i];
-        i++;
-    }
+	while (src[i])
+	{
+		if (src[i] != '\0')
+			dest[destLen + i] = src[i];
+		i++;
+	}
 	dest[destLen + i] = '\0';
 
 	return (dest);
