1b949f874f202e9555dd639d976b6e71ae7677d9
Roxxion <hiojam2016@gmail.com>
2023-02-28 08:44:40 -0800

    Task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array
+ *
+ * @a: *int
+ * @n: int
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int b[n];
+
+	for (i = n; i >= 0; i--)
+		b[(n - 1) - i] = a[i];
+
+	a = b;
+}
