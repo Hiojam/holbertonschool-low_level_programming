e86fabeac08e952dc172b836d90a7970f3f311cb
Roxxion <hiojam2016@gmail.com>
2023-02-28 05:57:35 -0800

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,14 +14,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[i])
 	{
-		if (i + 1 > n)
+		if (i > n)
 			break;
 
 		if (src[i] != '\0')
 			dest[i] = src[i];
 		i++;
 	}
-	/*dest[i] = '\0';*/
 
 	return (dest);
 }
