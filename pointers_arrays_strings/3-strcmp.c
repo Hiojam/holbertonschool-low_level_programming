20c32a8c1f2d773761905da3b5d7940a2a31d60b
Roxxion <hiojam2016@gmail.com>
2023-02-28 06:38:07 -0800

    Task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ *
+ * @s1: *char
+ * @s2: *char
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int s1Len = getASCIIValue(s1), s2Len = getASCIIValue(s2);
+
+	if (s1Len > s2Len)
+		return (s1Len - s2Len);
+	else if (s1Len < s2Len)
+		return (-(s1Len - s2Len));
+
+	return (0);
+}
+
+/**
+ * getASCIIValue - Get the ascii value of a *char
+ *
+ * @array: *char
+ * Return: int
+ */
+int *getASCIIValue(char *array)
+{
+	char c;
+	int i, ascii;
+	int result[100];
+
+	for (i = 0; array[i]; i++)
+	{
+		ascii = (int) array[i];
+		result[i] = ascii;
+	}
+
+	return (result);
+}
