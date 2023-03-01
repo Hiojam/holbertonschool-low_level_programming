7c563b87deb56f374b7690d42201516d7936819f
Roxxion <hiojam2016@gmail.com>
2023-03-01 09:28:35 -0800

    Task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int get_firstsegment(char *s);
+
 /**
  * _strspn - Gets the length of a prefix substring
  *
@@ -9,15 +11,37 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, matches = 0;
+	unsigned int i = 0, matches = 0, max = get_firstsegment(s);
 	int z = 0;
 
 	for (; s[i]; i++)
 	{
+		if (i == max)
+			return (matches);
+
 		for (z = 0; accept[z]; z++)
 			if (s[i] == accept[z])
 				matches++;
 	}
 
 	return (matches);
+}
+
+/**
+ * get_firstsegment - Get first segment of a *char
+ *
+ * @s: *char
+ * Return: int
+ */
+int get_firstsegment(char *s)
+{
+	int i = 0, x = 0;
+	char a[] = {' ', '	', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (; s[i]; i++)
+		for (; a[x]; x++)
+			if (s[x] == s[i])
+				return (i);
+
+	return (0);
 }
