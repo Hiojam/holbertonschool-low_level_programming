ada452e4d9b6c8f6bd3c8ff4e6ea31a6aeb3b53b
Roxxion <hiojam2016@gmail.com>
2023-03-01 06:56:47 -0800

    Task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ *
+ * @s: *char
+ * Return: *char
+ */
+char *leet(char *s)
+{
+	int i = 0, z = 0;
+	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'}
+	char replace[] = {'4', '3', '0', '7', '1'}
+
+	for (; s[i]; i++)
+	{
+		for (; letters[z]; z++)
+		{
+			if (letters[z] == s[i])
+				s[i] = replace[z / 2];
+		}
+	}
+	return (s);
+}
