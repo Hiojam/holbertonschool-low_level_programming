120ea6089cdc3952a0db5eebb245899032a76fb1
Roxxion <hiojam2016@gmail.com>
2023-03-06 04:44:01 -0800

    Task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ *
+ * @n: *char
+ * Return: void
+ */
+void _puts_recursion(char *n)
+{
+	unsigned int i = 0;
+
+	if (n[i])
+	{
+		_putchar(n[i]);
+		i++;
+		_puts_recursion(s + i);
+	}
+	else
+		_putchar('\n');
+	
+}
