7d8b1f98dabb1a77ad9ac5f8025ea7efb1cb6276
Roxxion <hiojam2016@gmail.com>
2023-03-06 09:07:15 -0800

    Task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /**
  * main - Adds positive numbers
@@ -11,7 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int total = 0, i = 1;
+	int total = 0, i = 1, x = 0;
 
 	if (argc < 3)
 	{
@@ -21,13 +22,14 @@ int main(int argc, char *argv[])
 
 	for (; i < argc; i++)
 	{
-		if (atoi(argv[i]) != 0)
-			total += atoi(argv[i]);
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		for (x = 0; argv[i][x]; x++)
+			if (!isdigit(argv[i][x]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		total += atoi(argv[i]);
 	}
 
 	printf("%d\n", total);
