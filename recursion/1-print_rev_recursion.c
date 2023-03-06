1bf7479b99d6528238b857528c546c84d57b81f3
Roxxion <hiojam2016@gmail.com>
2023-03-06 04:58:59 -0800

    Task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ *
+ * @n: char *s
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	unsigned int i = 0;
+
+	if (s[i])
+	{
+		_print_rev_recursion(s + i + 1);
+		putchar(s[i]);
+		i++;
+	}
+}
