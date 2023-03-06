c8c6c67a9f995d4cc42760c412a09f5a6d2a41ae
Roxxion <hiojam2016@gmail.com>
2023-03-06 05:44:45 -0800

    Task 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,15 +3,17 @@
 /**
  * _pow_recursion - Returns the value of x raised to the power of y
  *
- * @s: int x, int y
+ * @x: int
+ * @y: int
  * Return: The value of x raised to the power of y
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
+
 	if (y == 0)
-		return (0);
+		return (1);
 
 	return (x * _pow_recursion(x, y - 1));
 }
