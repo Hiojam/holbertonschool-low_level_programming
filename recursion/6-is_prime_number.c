1cab921f722b20ae991378f6529e8bf8192fdcce
Roxxion <hiojam2016@gmail.com>
2023-03-06 06:28:05 -0800

    Task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int prime(int num, int i);
+
+/**
+ * is_prime_number - Checks if the param is a prime number
+ *
+ * @n: int n
+ * Return: Returns 1 if the input int is a prime number, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	int a = prime(n, n / 2);
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+		return (1);
+
+	return (a);
+}
+
+/**
+ * prime - Counts the prime num
+ *
+ * @num: int
+ * @i: int
+ * Return: The prime boolean.
+ */
+int prime(int num, int i)
+{
+	if (i == 1)
+		return (1);
+
+	if (num % i == 0)
+		return (0);
+
+	return prime(num, i - 1);
+}
