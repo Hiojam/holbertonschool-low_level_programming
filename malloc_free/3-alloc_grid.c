bc78461c27b9363ecb980dab47ad8a824b205277
Roxxion <hiojam2016@gmail.com>
2023-03-13 11:05:17 -0700

    Task 3: freed

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -23,6 +23,15 @@ int **alloc_grid(int width, int height)
 	for (y = 0; y < height; y++)
 	{
 		grid[y] = malloc(sizeof(int) * width);
+
+		if (grid[y] != NULL)
+			continue;
+
+		for (y--; y >= 0; y--)
+			free(grid[y]);
+
+		free(grid);
+		return (NULL);
 	}
 
 	for (y = 0; y < height; y++)
