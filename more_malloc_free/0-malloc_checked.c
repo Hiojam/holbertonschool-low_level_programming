ab2152ab76a357e9a1e67608606cab527e12c9a0
Roxxion <hiojam2016@gmail.com>
2023-03-14 10:03:30 -0700

    Task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - Allocates memory using malloc
+ *
+ * @b: unsigned int
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *s = malloc(b);
+
+	if (s == NULL)
+	{
+		exit(98);
+		return (NULL);
+	}
+	
+	return (s);
+}
---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *string_nconcat - Concatenates two strings
+ *
+ * @n: char *s1, char *s2, unsigned int n
+ * Return: A pointer to a newly allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	return ();
+}
