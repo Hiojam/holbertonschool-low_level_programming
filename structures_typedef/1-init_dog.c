55d038ea3647f42c146d469cc1ba24d0fade8506
Roxxion <hiojam2016@gmail.com>
2023-03-20 10:06:26 -0700

    Task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,12 +3,17 @@
 /**
  * init_dog - Initializes a variable of type struct dog
  *
- * @n: struct dog *d, char *name, float age, char *owner
+ * @d: struct *dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL) return;
 
-	struct dog d = {name, age, owner};
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
 }
