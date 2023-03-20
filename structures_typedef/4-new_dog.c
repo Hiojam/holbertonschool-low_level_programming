385ec8e1a4efa7ec9c60cd7f120a5e4b7725a966
Roxxion <hiojam2016@gmail.com>
2023-03-20 10:47:08 -0700

    Task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * *new_dog - Creates a new dog
+ *
+ * @name: *char
+ * @age: float
+ * @owner: *char
+ * Return: The dog or Null if it fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+
+	if (!name || !owner)
+		return (NULL);
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
