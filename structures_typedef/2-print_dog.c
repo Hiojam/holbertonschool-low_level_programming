0fcba70bccad31f9abb5c1c8caca6101ea404661
Roxxion <hiojam2016@gmail.com>
2023-03-20 10:15:22 -0700

    Task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * print_dog - Prints a struct dog
@@ -8,5 +9,18 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %f.1\nOwner: %s", d->name, d->age, d->owner);
+	char *name = d->name;
+	char *owner = d->owner;
+	float age = d->age;
+
+	if (d == NULL)
+		return;
+
+	if (name == NULL)
+		name = "(nil)";
+
+	if (owner == NULL)
+		owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s", name, age, owner);
 }
