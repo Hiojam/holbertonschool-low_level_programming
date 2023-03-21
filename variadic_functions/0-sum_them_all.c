e3d808098fba92cd628d35bea00519fd4df6d002
Roxxion <hiojam2016@gmail.com>
2023-03-21 11:52:33 -0700

    Task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * sum_them_all - Adds numbers
+ *
+ * @n: const unsigned int
+ * Return: The sum of all its parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int resul = 0;
+	va_list args;
+	unsigned int i = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (; i < n; i++)
+		resul += va_arg(args, int);
+
+	va_end(args);	
+	return (resul);
+}
