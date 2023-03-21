8b24862641efce20151e792291f0833be9dad423
Roxxion <hiojam2016@gmail.com>
2023-03-21 11:58:17 -0700

    Task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers
+ *
+ * @separator: const *char
+ * @n: const unsigned int
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	int resul = 0;
+	va_list args;
+	unsigned int i = 0;
+
+	if (n == 0)
+		return;
+
+	if (separator == NULL)
+		return;
+
+	va_start(args, n);
+
+	for (; i < n; i++)
+		printf("%d%s", va_arg(args, int));
+
+	_putchar('\n');
+
+	va_end(args);
+	return (resul);
+}
