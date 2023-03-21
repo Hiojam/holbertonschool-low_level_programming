d498e4de272beef306da3fb9a72fd52fedc9c3d9
Roxxion <hiojam2016@gmail.com>
2023-03-21 11:59:44 -0700

    Task 1: fixed some issues

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,10 +22,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 
 	for (; i < n; i++)
-		printf("%d%s", va_arg(args, int));
+		printf("%d%s", va_arg(args, int), separator);
 
-	_putchar('\n');
+	putchar('\n');
 
 	va_end(args);
-	return (resul);
 }
