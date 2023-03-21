c399ce1644d377744f219c8c61cf8f319bebfebc
Roxxion <hiojam2016@gmail.com>
2023-03-21 12:03:59 -0700

    Task 1: fixed some use cases

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,7 +18,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 
 	for (; i < n; i++)
-		printf("%d%s", va_arg(args, int), separator);
+	{
+		printf("%d", va_arg(args, int), separator);
+		if (i + 1 != n)
+			printf("%s", separator);
+	}
 
 	putchar('\n');
 
