2842156206f3c9b317e739c4658214406a098183
RoxxionYT <hiojam2016@gmail.com>
2023-04-06 09:51:49 -0700

    Task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - Counts the number of elements in a linked list.
+ *
+ * @h: list_t
+ * Return: The number of elements in a list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	int length = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
