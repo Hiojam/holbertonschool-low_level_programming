b2f75b50e10e06840b5b9945734dc8170fdc76da
RoxxionYT <hiojam2016@gmail.com>
2023-04-08 06:18:35 -0700

    Task 0: fixed betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * print_dlistint - Prints all the elements of a dlistint_t list.
  *
- * @param: desc
+ * @h: dlistint_t
  * Return: The number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
