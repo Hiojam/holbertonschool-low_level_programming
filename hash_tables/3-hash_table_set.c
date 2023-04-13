7dd28222b444de07563a8acdc62b220dd417e75a
RoxxionYT <hiojam2016@gmail.com>
2023-04-13 06:11:17 -0700

    Task 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ *
+ * @ht: hash_table_t
+ * @key: const char
+ * @value: const char
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL && strcmp(key, ht->array[index]->key) == 0)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
