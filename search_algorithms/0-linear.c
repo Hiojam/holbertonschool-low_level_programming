b3b65fcba7e316aee7cf00222874f351b0d505fd
Hiojam <hiojam2016@gmail.com>
2023-05-23 09:18:40 -0500

    Task 0: used size

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
     if (!array)
         return (-1);
 
-    for (; array[i]; i++)
+    for (; i < size; i++)
     {
         printf("Value checked array[%d] = [%d]\n", i, array[i]);
         if (array[i] == value)
