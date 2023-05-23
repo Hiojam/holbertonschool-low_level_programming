cb74fb8fbd2eb6f52e2f614574042656b91e3382
Hiojam <hiojam2016@gmail.com>
2023-05-23 09:25:44 -0500

    Task 0: fixed spaces

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,16 +12,16 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    for (; i < size; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
